46da995e7ac16bcfb5b25244ce06809d1f960a49
David Calavera <david.calavera@gmail.com>
2016-02-10 18:24:54 -0800

    :sunrise:
    Signed-off-by: David Calavera <david.calavera@gmail.com>

---
+++ README.md
@@ -0,0 +1,5 @@
+# Introduction
+
+Chrome extension to add extra buttons to GitHub's notifications pages.
+
+![mute-buttons](https://cloud.githubusercontent.com/assets/1050/12968197/69d2a7dc-d023-11e5-9a10-635942297b0d.gif)
---
+++ src/inject/inject.js
@@ -0,0 +1,19 @@
+(function() {
+    var readyStateCheckInterval = setInterval(function() {
+      if (document.readyState === "complete") {
+        clearInterval(readyStateCheckInterval);
+
+        var navigationTab = ".notification-center .tabnav .right"
+        $(navigationTab).append('<a href="#mute_notifications" class="btn btn-sm mute_closed">Mute closed issues</a>')
+        $(navigationTab).append('<a href="#mute_notifications" class="btn btn-sm mute_merged" style="margin-left: 3px;">Mute merged PRs</a>')
+
+        $(".mute_merged").click(function() {
+          $(".type-icon-state-merged").parent().siblings().find(".mute button").click()
+        });
+
+        $(".mute_closed").click(function() {
+          $(".type-icon-state-closed").parent().siblings().find(".mute button").click()
+        });
+      }
+    }, 10);
+})();
